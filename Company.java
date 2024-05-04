20c877bdbfef148043f09d643fe73a4698c8ea7d
Magdalena Zakrzewska <magdazakrzewskamagda@gmaill.com>
2024-05-04 14:45:35 +0200

    chore: add file

--- Company.java
+++ Company.java
@@ -12,7 +12,19 @@ public class Company {
         employees[3] = new Employee("Jan Kwiatkowski", 7400);
         employees[4] = new Worker("Mariusz Kowalski", 3900, "Analyst");
 
+        int notManager = 0;
+        for (Employee employee : employees) {
+            if (!(employee instanceof Manager)) {
+                notManager++;
+            }
+        }
+
         
+        ((Manager)employees[0]).setNumberOfSubordinates(notManager);
+
+        
+        employees[0].setSalary(7500);
+
         System.out.println("Dane dla wszystkich pracowników:");
         for (Employee employee : employees) {
             System.out.println(employee);
